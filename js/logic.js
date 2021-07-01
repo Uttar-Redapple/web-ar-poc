50da03f8778c2528b55a59d573d2fa3180b99eb3
Uttar-Redapple <uttar@redappletech.com>
2021-07-01 12:29:00 +0530

    fix

--- js/logic.js
+++ js/logic.js
@@ -4,6 +4,10 @@ AFRAME.registerComponent('modify-materials', {
          console.log('model-loaded!!');
          const obj = this.el.getObject3D('mesh');
          console.log('obj',obj);
+         const aEntity = document.querySelector("#animated-model");
+         aEntity.addEventListener('click',()=>{
+          el.object3D.rotation.x += Math.PI;
+         })
       });
   }
   });
