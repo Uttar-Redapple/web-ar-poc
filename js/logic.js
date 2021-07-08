3f880082df37f5f0300ef6cc55d8eafc609d9bf9
Uttar-Redapple <uttar@redappletech.com>
2021-07-08 18:37:01 +0530

    new model

--- js/logic.js
+++ js/logic.js
@@ -1,13 +1,7 @@
-AFRAME.registerComponent('modify-materials', {
-    init: function () {
-      this.el.sceneEl.addEventListener('model-loaded', () => {
-         console.log('model-loaded!!');
-         const obj = this.el.getObject3D('mesh');
-         console.log('obj',obj);
-         const aEntity = document.querySelector("#animated-model");
-         aEntity.addEventListener('click',()=>{
-          el.object3D.rotation.x += Math.PI;
-         })
-      });
-  }
-  });
+AFRAME.registerComponent('modify-materials', { 
+   init: function() {   
+    //   this.el.addEventListener('touchend', handleClickEvent);  
+      this.el.addEventListener('click', (evt) => {
+        alert('This 3D element was clicked!');
+      }); 
+   } });
