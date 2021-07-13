c2cf18bbe679a91f6cb658834156dc0ca366a358
Uttar-Redapple <uttar@redappletech.com>
2021-07-13 21:22:45 +0530

    new model

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,6 @@ AFRAME.registerComponent('modify-materials', {
    init: function() {   
     //   this.el.addEventListener('touchend', handleClickEvent);  
       this.el.addEventListener('click', (evt) => {
-        alert('This 3D element was clicked!');
+        console.log('This 3D element was clicked!');
       }); 
    } });
